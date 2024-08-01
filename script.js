78aaf36ba2dbc0506345612beed8f1558c12f7ef
datpham0412 <tiendat041202@gmail.com>
2024-08-01 10:02:07 +1000

    Masked Password function

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+function maskPassword(pass) {
+  let str = "";
+  for (let index = 0; index < pass.length; index++) {
+    str += "*";
+  }
+  return str;
+}
+
 document.querySelector(".btn").addEventListener("click", (e) => {
   e.preventDefault();
   console.log("Hello feng");
   console.log(username.value, password.value);
+
+  // Mask the password
+  let maskedPassword = maskPassword(password.value);
+  console.log("Masked password: ", maskedPassword);
+
   let passwords = localStorage.getItem("passwords");
   console.log(passwords);
   if (passwords == null) {
