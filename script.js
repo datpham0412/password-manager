6fb1826336e0e17c7caaedceccd315658221b713
datpham0412 <tiendat041202@gmail.com>
2024-08-01 17:21:02 +1000

    feat: Delete password function

--- script.js
+++ script.js
@@ -22,6 +22,17 @@ function copyText(txt) {
   );
 }
 
+const deletePassword = (website) => {
+  let data = localStorage.getItem("passwords");
+  let arr = JSON.parse(data);
+  arrUpdated = arr.filter((e) => {
+    return e.website != website;
+  });
+  localStorage.setItem("passwords", JSON.stringify(arrUpdated));
+  alert(`Successfully deleted ${website}'s password`);
+  showPasswords();
+};
+
 document.querySelector(".btn").addEventListener("click", (e) => {
   e.preventDefault();
   console.log("Hello feng");
